e9d19a11c7d4a512dc8b9eaaa879ba0e4de98be8
Melina Owrang <Melina.Owrang@edu.mayk.fi>
2025-03-24 15:35:57 +0200

    seuraava versio

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+import random
+import time
 
 app = Flask(__name__)
+lista=["🍒", "🍋", "🔔", "🍉", "⭐", "7️⃣", "🎰"]
 
 @app.route("/")
 def root():
@@ -8,5 +11,38 @@ def root():
 
 @app.route("/vastaus")
 def vastaus():
-    return render_template('vastaus.html', nimi=request.args['nimi'])
 
+    a=random.choice(lista)
+
+    b=random.choice(lista)
+
+    c=random.choice(lista)
+    
+    time.sleep(2)    
+    
+    if a==b and b==c:
+        win= True
+    else:
+        win= False
+    return render_template('vastaus.html', bet=int(request.args['money']), a=a, b=b, c=c, win=win)
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
+    
+    
+    
+    
+#<h2>Your account balance is {{balance}} dollars.</h2>
